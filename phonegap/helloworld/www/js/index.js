ac60a2f421fc6972ce300aecaafd3cc7aab99bae
shameer.ali <shameer.ali@jifflenow.com>
2017-08-23 11:36:32 +0530

    init

---
+++ phonegap/helloworld/www/js/index.js
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+        document.addEventListener("backbutton", onBackKeyDown, false);
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        initialize();
+        console.log('Received Event: ' + id);
+    }
+};
+
+function initialize() {
+  $(document).ready(function(){
+    $('.auto').on('click', displayAutoDrivers)
+    $('.clublogo').show()
+    $('.container').hide()
+    setTimeout(function() {
+      $('.clublogo').hide()
+      $('.container').show()
+      $('.phone-contents').hide()
+      $('body').css('background-color', 'white')
+    }, 3000)
+    function displayAutoDrivers() {
+      $('.phone-contents').show()
+      $('.main-contents').hide()
+    }
+  })
+
+}
+
+
+function onBackKeyDown () {
+  $('.clublogo').hide()
+  $('.phone-contents').hide()
+  $('.main-contents').show()
+}
